d39415bb9272dfae455d4dd17dc3cd24dea34ffa
Danny213123 <73540329+Danny213123@users.noreply.github.com>
2020-12-02 21:03:58 -0500

    Update 2020 Day 2 Part 2.py

--- 2020 Day 2 Part 2.py
+++ 2020 Day 2 Part 2.py
@@ -1,5 +1,5 @@
 r = []
-for q in range (1):
+for q in range (1000):
   k = (input())
   k = k.replace("-", " ")
   k = k.replace(":", "")
@@ -11,6 +11,9 @@ for x in range (len(r)):
   counter = 0
   Min, Max, Letter, String = r[x][0], r[x][1], r[x][2], r[x][3]
   if (String[int(Min) - 1] == Letter or String[int(Max) - 1] == Letter):
-    valid += 1
+    if (String[int(Min) - 1] == Letter and String[int(Max) - 1] == Letter):
+      pass
+    else:
+      valid += 1
 
 print(valid)
